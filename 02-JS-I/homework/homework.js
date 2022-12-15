2fc4692e97d6f467044c36392e4550996bff0a5b
pablochuck <119815364+pablochuck@users.noreply.github.com>
2022-12-15 14:10:28 -0300

    Update homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = 'hola';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
@@ -246,4 +246,3 @@ module.exports = {
   areaDelTriangulo,
   deEuroAdolar,
   esVocal,
-};
